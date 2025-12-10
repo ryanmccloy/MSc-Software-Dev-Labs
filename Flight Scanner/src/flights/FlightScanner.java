1d61c0d0b7ca3dec15a41a6a4822ed12acc3d0f7
Ryan McCloy <ryannmccloy@gmail.com>
2025-12-10 14:07:22 +0000

    Simple app to calculate the average grade of my course

---
+++ Flight Scanner/src/flights/FlightScanner.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package flights;
+
+/**
+ * 
+ */
+public class FlightScanner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
